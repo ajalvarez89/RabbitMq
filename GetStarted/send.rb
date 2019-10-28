2280de65e73a35597d683194977978a746526445
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-10-28 14:36:32 -0500

    adding send and receive payload

--- GetStarted/send.rb
+++ GetStarted/send.rb
@@ -9,7 +9,7 @@ channel = connection.create_channel
 
 queue = channel.queue('hello')
 
-channel.default_exchange.publish('Hello World!', routing_key: queue.name)
+channel.default_exchange.publish('Hello World Alvaro!', routing_key: queue.name)
 puts " [x] Sent 'Hello World!' for the quote: #{queue.name}"
 
 connection.close
